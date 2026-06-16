95e96730c16b577466a8d5f495aa2cc245fe5b64
GIS-GEORGIA <kapan.gio777@gmail.com>
2026-06-16 21:55:28 +0400

    debug: add global excepthook to show crash traceback in dialog

--- standalone/app.py
+++ standalone/app.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import traceback
 
 
 def main():
@@ -9,7 +10,7 @@ def main():
     if root not in sys.path:
         sys.path.insert(0, root)
 
-    from PyQt6.QtWidgets import QApplication
+    from PyQt6.QtWidgets import QApplication, QMessageBox
     from PyQt6.QtCore import Qt
     from PyQt6.QtGui import QFont
 
@@ -21,6 +22,13 @@ def main():
     app.setOrganizationName("GIS-GEORGIA")
     app.setOrganizationDomain("gis-georgia.ge")
     app.setApplicationVersion("1.0.0")
+
+    def _excepthook(exc_type, exc_value, exc_tb):
+        msg = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+        print(msg, file=sys.stderr)
+        QMessageBox.critical(None, "Unhandled Error", msg[:2000])
+
+    sys.excepthook = _excepthook
 
     from postgis_manager.utils import config, theme, i18n
     i18n.load(config.get("language", "en"))
